2aa2cbb7fd19aa8a585028ea76702353c51a3c4f
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-06-12 23:24:27 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,98 @@
+const todo_div_container = document.querySelector('#todo-div-container')
+const submit = document.querySelector('#input')
+const js_img = document.querySelector('#js_theme')
+const bgcolor = document.querySelector('.div-container')
+const cabeca = document.querySelector('#cabeca')
+const input_div = document.querySelector('#input-div')
+const input_div_c = document.querySelector('.input-div')
+const color_white = document.querySelector('.color-white')
+const fraco = document.querySelectorAll('.fraco')
+let v = 0
+let x = 1
+let classe = 'todo-div-color-black'
+let cond = false
+let numeros = 0
+let numeros2 = 10
+let numeros3 = 20
+let numeros4 = 30
+let itens = 0
+js_img.addEventListener('click', function () {
+    ++x
+    if (x % 2 == 0) {
+        js_img.src = './images/icon-moon.svg'
+        bgcolor.classList.replace('div-container-color-black', 'div-container-color-white')
+        cabeca.classList.replace('cabeca-dark', 'cabeca-white')
+        input_div.classList.replace('input-div-color-black', 'input-div-color-white')
+        input_div_c.classList.replace('todo-div-color-black', 'todo-div-color-white')
+        color_white.classList.replace('color-white', 'color-black')
+        for (let i = 0; i < fraco.length; i++) {
+            fraco[i].classList.replace('fraco', 'forte')
+        }
+        classe = 'todo-div-color-white'
+        cond = false
+        const div = document.querySelectorAll('.todo-div')
+        for (let i = 0; i < numeros; i++) {
+            div[i].classList.replace('todo-div-color-black', 'todo-div-color-white')
+        }
+    } else {
+        js_img.src = './images/icon-sun.svg'
+        bgcolor.classList.replace('div-container-color-white', 'div-container-color-black')
+        cabeca.classList.replace('cabeca-white', 'cabeca-dark')
+        input_div.classList.replace('input-div-color-white', 'input-div-color-black')
+        input_div_c.classList.replace('todo-div-color-white', 'todo-div-color-black')
+        color_white.classList.replace('color-black', 'color-white')
+        for (let i = 0; i < fraco.length; i++) {
+            fraco[i].classList.replace('forte', 'fraco')
+        }
+        classe = 'todo-div-color-black'
+        cond = true
+        const div = document.querySelectorAll('.todo-div')
+        for (let i = 0; i < numeros; i++) {
+            div[i].classList.replace('todo-div-color-white', 'todo-div-color-black')
+        }
+    }
+})
+submit.addEventListener('keypress', function (e) {
+    if (e.keyCode == 13) {
+        if (submit.value === '') {
+            return
+        }
+        ++numeros
+        ++numeros2
+        ++numeros3
+        ++numeros4
+        ++itens
+        $('#itens-left').html(itens)
+        const div = document.createElement('div')
+        div.classList.add('todo-div')
+        div.id = numeros
+        if (cond) {
+            div.classList.add(classe)
+        } else {
+            div.classList.add(classe)
+        }
+        todo_div_container.append(div)
+        const valor = submit.value
+        div.style.display = 'flex'
+        let divc = document.createElement('div')
+        divc.classList.add('todo-div-display')
+        div.append(divc)
+        divc.innerHTML = `<div class="first-div main-d1"> <div class="ball" id='${numeros3}'></div> </div> <div class="second-div main-d2"><p class="no-checked-p" id='${numeros4}'>
+        ${valor} </p> </div> <div class="last-div main-d3"> <img src="./images/icon-cross.svg" class="fechar" id='${numeros2}'> </div> `
+        submit.value = ''
+        const fechar = document.getElementById(numeros2)
+        fechar.addEventListener('click', function () {
+            document.getElementById(div.id).remove()
+            --itens
+            $('#itens-left').html(itens)
+
+        })
+        const ball = document.getElementById(numeros3)
+        const pa = document.getElementById(numeros4)
+        ball.addEventListener('click', function () {
+            document.getElementById(ball.id).classList.toggle('checked')
+            document.getElementById(pa.id).classList.toggle('checked-p')
+        })
+    }
+})
+
